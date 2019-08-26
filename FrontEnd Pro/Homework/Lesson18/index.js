e93d3d838615c3add65d1c463201a0a989c471da
Artem Kornev <artemkakornev1@gmail.com>
2019-08-26 21:27:21 +0300

    ,

--- FrontEnd Pro/Homework/Lesson18/index.js
+++ FrontEnd Pro/Homework/Lesson18/index.js
@@ -14,21 +14,22 @@ class Model {
     return [...this.state];
   }
 
-  changeStudent(studentNames) {
+  changeStudent() {
     const oldStudentsList = this.getState();
 
-    const [oldStudent, newStudent] = studentNames;
+    const oldStudentName = document.getElementById('old_student_name').value;
+    const newStudentName = document.getElementById('new_student_name').value;
 
-    const idx = oldStudentsList.indexOf(oldStudent);
+    const idx = oldStudentsList.indexOf(oldStudentName);
 
     let newStudentsList;
 
     if (idx === -1) {
-      newStudentsList = [...oldStudentsList, newStudent];
+      newStudentsList = [...oldStudentsList, newStudentName];
     } else {
       newStudentsList = [
         ...oldStudentsList.slice(0, idx),
-        newStudent,
+        newStudentName,
         ...oldStudentsList.slice(idx + 1),
       ];
     }
@@ -38,35 +39,24 @@ class Model {
 }
 
 class Controller {
-  init(model, view) {
+  init(model, View) {
     this.model = model;
-    this.view = view;
+    this.View = View;
 
-    view.render(model.getState());
+    this.View.render(model.getState());
   }
 
-  changeData(data) {
-    this.model.changeStudent(data);
+  replaceStudents() {
+    this.model.changeStudent();
   }
 
   onStateUpdate() {
-    this.view.render(this.model.getState());
+    this.View.render(this.model.getState());
   }
 }
 
 class View {
-  constructor(controller) {
-    this.controller = controller;
-  }
-
-  onBtnClick() {
-    const oldStudentName = document.getElementById('old_student_name').value;
-    const newStudentName = document.getElementById('new_student_name').value;
-
-    this.controller.changeData([oldStudentName, newStudentName]);
-  }
-
-  render(data) {
+  static render(data) {
     const list = document.getElementById('students_list');
 
     list.innerHTML = data.map((el) => `<li>${el}</li>`).join('');
@@ -74,10 +64,9 @@ class View {
 }
 
 const controller = new Controller();
-const view = new View(controller);
 const model = new Model(controller);
 
-controller.init(model, view);
+controller.init(model, View);
 
 const btn = document.getElementById('btn');
-btn.addEventListener('click', () => view.onBtnClick());
+btn.addEventListener('click', () => controller.replaceStudents());
